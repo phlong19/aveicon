87f74694da81dd8cd837d113b792a968657b863d
hagen.pham <hagen.pham@avepoint.com>
2024-10-08 15:41:13 +0700

    chore: finish and ready for publish

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: path.resolve(__dirname, "src/main.ts"),
+      name: "aveicon",
+      //   fileName: (format) => `aveicon.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+});
